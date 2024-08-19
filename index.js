3c92d4f9eac0ef7f34e3a2f9bd08d7d7cc431ab7
Mraasith <mohamed.raasith@cargostudio.co>
2024-08-20 01:57:04 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const counterBtn = document.getElementById("counterBtn");
+const randomNum = document.getElementById("RandNumBtn");
+
+counterBtn.onclick = function () {
+  window.location.href = "tutorialCodes/Counter/counter.html";
+};
+
+randomNum.onclick = function () {
+  window.location.href = "tutorialCodes/RandomNumber/randomNum.html";
+};
---
+++ tutorialCodes/RandomNumber/randomNum.js
@@ -0,0 +1,28 @@
+const rollBtn = document.getElementById("rollBtn");
+const genNum = document.getElementById("genNum");
+const minNum = document.getElementById("minNum");
+const maxNum = document.getElementById("maxNum");
+const backBtn = document.getElementById("backBtn");
+
+max = 10;
+min = 0;
+
+rollBtn.onclick = function () {
+  console.log(minNum.value);
+  console.log(maxNum.value);
+  min = Number(minNum.value);
+  max = Number(maxNum.value);
+  if (min > max) {
+    max = min + 1;
+    maxNum.value = max;
+    window.alert(`Max smaller than min, set max to ${max}.`);
+  }
+
+  rando = Math.floor(Math.random() * max) + min;
+  console.log(max, min, rando);
+  genNum.textContent = rando;
+};
+
+backBtn.onclick = function () {
+  window.location.href = "../../index.html";
+};
